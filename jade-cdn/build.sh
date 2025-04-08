6859910f87d5ee5fdebe47ed218eca61abbf4692
Jade Shan <evokeralucard@gmail.com>
2025-04-08 19:48:39 +0800

    update

--- jade-cdn/build.sh
+++ jade-cdn/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cd cdn-libs/3rd/
-bash ./build.sh
-cd ../..
+# package 3rd-cdn
+echo $PWD
+echo "package 3rd-lib : start"
+mkdir -p ./webroot/3rd/ 
+rm   -rf ./webroot/3rd/*   
+cp -r cdn-libs/3rd/* ./webroot/3rd
+tar czf 3rd.tar.gz webroot/3rd/
+echo "package 3rd-lib : end"
 echo $PWD
 
+# package jade-utils
 cd cdn-libs/jadeutils.v2/
 bash ./build.sh
 echo $PWD
