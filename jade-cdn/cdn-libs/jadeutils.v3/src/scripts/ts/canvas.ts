8116c96e6cb65ec483f651f5ca4e841ac8752f9b
Jade Shan <evokeralucard@gmail.com>
2025-07-29 19:19:04 +0800

    update

---
+++ jade-cdn/cdn-libs/jadeutils.v3/src/scripts/ts/canvas.ts
@@ -0,0 +1,19 @@
+import { Geo2D, Geo2DUtils, Point2D } from './geo2d.js';
+
+export class Canvas2DShape<T extends Geo2D> {
+	shape: T;
+
+	constructor(id: string, shape: T, cvsCtx: CanvasRenderingContext2D) {
+		//let cck: HTMLCanvasElement = document.getElement();
+		//let cvsCtx: CanvasRenderingContext2D = cck.getContext("2d");
+
+	}
+
+} 
+
+
+export namespace CanvasUtils {
+
+
+
+}
