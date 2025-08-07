c6e2ec3d30e070fbf7b8788c169acb52bbcee8a9
Jade Shan <evokeralucard@gmail.com>
2025-08-07 21:26:03 +0800

    update

--- jade-cdn/cdn-libs/jadeutils.v3/src/scripts/ts/UIWindow.ts
+++ jade-cdn/cdn-libs/jadeutils.v3/src/scripts/ts/UIWindow.ts
@@ -1,8 +1,18 @@
+import { SimpleMap } from "./dataStructure.js";
+import { IPoint2D } from "./geo2d.js";
+
+
+export class UIDesktopStatus {
+	allWindows: SimpleMap<string,UIObj> = new SimpleMap();
+	newWindowPopPosition: IPoint2D = {x: 20, y: 20};
+	constructor(){}
+}
+
 
 export interface UIObj {
 	readonly id: string;
 
-	renderIn(elem: HTMLElement): void;
+	renderIn(desktop: UIDesktopStatus, parentElem: HTMLElement): void;
 }
 
 // 不可以调整大小的UI组件
@@ -19,19 +29,19 @@ export class UIWindow implements ResizeableUI {
 	readonly id: string;
 
 	constructor(id: string) {
-		this.id = id;
+		this.id = `${id}-${(new Date()).getTime()}`;
 	}
 
-	renderIn(elem: HTMLElement): void {
-		let div = JadeWindowUI.renderWindowTplt(this.id);
-		elem.appendChild(div);
+	renderIn(desktop: UIDesktopStatus, parentElem: HTMLElement): void {
+		let div = JadeWindowUI.renderWindowTplt(desktop, parentElem, this.id);
+		parentElem.appendChild(div);
 	}
 
 }
 
 export namespace JadeWindowUI {
 
-	export function renderWindowTplt(id: string): HTMLDivElement {
+	export function renderWindowTplt(desktop: UIDesktopStatus, parentElem: HTMLElement, id: string): HTMLDivElement {
 		let winTplt = `
 		<div class="title-bar">
 			<div class="title-bar-text">A Window With A Status Bar</div>
@@ -48,6 +58,27 @@ export namespace JadeWindowUI {
 				<li>A Notepad</li>
 				<li>Or even a File Explorer!</li>
 			</ul>
+
+			<div class="sunken-panel" style="height: 120px; width: 240px;">
+				<table class="interactive">
+					<thead>
+						<tr><th>Name</th><th>Version</th><th>Company</th></tr>
+					</thead>
+					<tbody>
+						<tr><td>MySQL ODBC 3.51 Driver</td><td>3.51.11.00</td><td>MySQL AB</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+						<tr><td>SQL Server</td><td>3.70.06.23</td><td>Microsoft Corporation</td></tr>
+					</tbody>
+				</table>
+			</div>
+
 		</div>
 		<div class="status-bar">
 			<p class="status-bar-field">Press F1 for help</p>
@@ -59,6 +90,16 @@ export namespace JadeWindowUI {
 		div.classList = "window";
 		div.style = "width: 320px"
 		div.innerHTML = winTplt;
+
+		// 
+		div.style.position = 'absolute';
+		div.style.left = `${desktop.newWindowPopPosition.x}px`;
+		div.style.top  = `${desktop.newWindowPopPosition.y}px`;
+		desktop.newWindowPopPosition = {
+			x: desktop.newWindowPopPosition.x + 20,
+			y: desktop.newWindowPopPosition.y + 20
+		}
+
 		return div;
 	};
 
--- jade-cdn/cdn-libs/jadeutils.v3/src/scripts/ts/testJadeUI.ts
+++ jade-cdn/cdn-libs/jadeutils.v3/src/scripts/ts/testJadeUI.ts
@@ -6,15 +6,18 @@
 //import { CanvasCircle2D, CanvasLine2D, CanvasPoint2D, CanvasRay2D, CanvasRectangle2D, CanvasUtils, ICanvasRay2D, ICanvasRectangle2D } from './canvas.js';
 //import { Geo2DUtils, GeoShape2D, IRay2D, Line2D, Point2D, Ray2D } from './geo2d.js';
 
-import { UIWindow } from "./UIWindow.js";
+import { UIDesktopStatus, UIWindow } from "./UIWindow.js";
 
 
 export class TestJadeUI {
 
 
 	static testWindowUI() {
-		let win = new UIWindow("test-ui-window");
-		win.renderIn(document.body);
+		let desktop: UIDesktopStatus = new UIDesktopStatus();
+		let win1 = new UIWindow("test-ui-window-01");
+		win1.renderIn(desktop, document.body);
+		let win2 = new UIWindow("test-ui-window-01");
+		win2.renderIn(desktop, document.body);
 	}
 
 
